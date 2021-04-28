dba55cd57b35d0472b231fbce43d4968e037beba
Jose Claudio Tomas Jr <jclaudiotomas@gmail.com>
2021-04-28 04:18:48 -0300

    "Ajustando Generator"

--- Concorrencia/Generator/genarator.go
+++ Concorrencia/Generator/genarator.go
@@ -14,16 +14,18 @@ func titulo(urls ...string) <-chan string {
 			resp, _ := http.Get(url)
 			html, _ := ioutil.ReadAll(resp.Body)
 
-			r, _ := regexp.Compile("<title>(.*?)<\\/titiel>")
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
 			c <- r.FindStringSubmatch(string(html))[1]
 		}(url)
 	}
+
 	return c
 }
 
 func main() {
-	t1 := titulo("https://www.udemy.com/", "http://www.stcruz.com.br/Paginas/Index.aspx")
-	t2 := titulo("https://www.alura.com.br/", "https://gobyexample.com/")
+
+	t1 := titulo("https://www.google.com.br/", "https://www.youtube.com/")
+	t2 := titulo("https://www.udemy.com/", "https://gobyexample.com/")
 
 	fmt.Println("Primeiros: ", <-t1, "|", <-t2)
 	fmt.Println("Segundos: ", <-t1, "|", <-t2)
